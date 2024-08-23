35fab92ed8676fc714033bf156061401d00555cf
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-08-23 10:16:05 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function mincost(arr)
 { 
+	let heap=arr.slice();
+	heap.sort((a,b)=>a-b);
+	let totalCost=0;
+	while(heap.length>1)
+		{
+			let firstCost=heap.shift();
+			let secondCost=heap.shift();
+			Cost=firstCost+secondCost;
+			totalCost=+Cost;
+			heap.push(Cost);
+			heap.sort((a,b)=>a-b);
+		}
 //write your code here
 // return the min cost
   
